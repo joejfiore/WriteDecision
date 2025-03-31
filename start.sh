dd691eda592f1a191f8c71f1af6ec0a1ddb75b29
Joe <joejfiore@gmail.com>
2025-03-31 19:20:35 -0400

    Initial commit of RAG system

--- start.sh
+++ start.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-# If PORT is not set, default to 8000
 PORT=${PORT:-8000}
-
-# Print the port for debugging
 echo "Starting server on port: $PORT"
-
-# Run with uvicorn directly
 exec uvicorn main:app --host 0.0.0.0 --port $PORT
